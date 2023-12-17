fb28ab7767551d2b7b0aeb514b946020f9c65e4b
yx <chewyanxin@gmail.com>
2023-12-17 21:13:06 +0800

    add new files

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import { Login, Signup } from "./Pages";
+import Home from "./Pages/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
