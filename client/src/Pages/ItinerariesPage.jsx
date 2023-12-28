de3472137729169244bc9057e65c5385641f32f9
ZENN\User <miljy123@gmail.com>
2023-12-29 00:06:54 +0800

    Create itineraries page to view trips created in table format

--- client/src/Pages/ItinerariesPage.jsx
+++ client/src/Pages/ItinerariesPage.jsx
@@ -1,18 +1,78 @@
 // my trips
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
 import axios from "axios";
+import NavBar from "../Components/Navbar";
 
 const ItinerariesPage = () => {
+    const [username, setUsername] = useState("");
+    const [cookies, removeCookie] = useCookies([]);
+
+    const [itineraries, setItineraries] = useState([]);
     const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setLoading(true);
+        axios
+            .get('http://localhost:4000/itineraries')
+            .then((response) => {
+                setItineraries(response.data.data);
+                setLoading(false);
+            })
+            .catch((error) => {
+                console.log(error);
+                setLoading(false);
+            });
+    }, []);
 
     const navigate = useNavigate();
 
+    const Logout = () => {
+        removeCookie("token");
+        navigate("/login");
+      };
+
     return (
-        <div className="p-4">
-            <h1 className="text-3xl my-4">Itineraries page</h1>
-            { loading ? <p>loading...</p> : ''}
+        <div className="home_page">
+            <div>
+        <NavBar user={username} logout={Logout}/>
+            </div>
+            <div className="p-4">
+                <h3>My Trips</h3>
+                <Link to='/itineraries/create'>
+                    + Add trip
+                </Link>
+            </div>
+            
+            { loading ? (
+                <p>loading...</p>
+                ) : (
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Title</th>
+                                <th>Country</th>
+                                <th>Start Date</th>
+                                <th>End Date</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {itineraries.map((itinerary, index) => (
+                                <tr key={(itinerary._id)}>
+                                    <td>{index + 1}</td>
+                                    <td>{itinerary.title}</td>
+                                    <td>{itinerary.country}</td>
+                                    <td>{itinerary.startDate}</td>
+                                    <td>{itinerary.endDate}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                )
+            }
         </div>
     )
 }
